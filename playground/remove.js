3708d1c49cc736900add8f7dce0c96bae00e9bd9
Muhammad Umar <umawtips1@gmail.com>
2018-04-06 05:50:21 +0500

    removing functionality is added

---
+++ playground/remove.js
@@ -0,0 +1,16 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect("mongodb://127.0.0.1:27017/userDataa", (error, db) => {
+  if (error) {
+    return console.log("Unable to connect to the database");
+  }
+  console.log("Connected to the database.");
+  
+  db.collection("Users").delete().then((result) => {
+        console.log(JSON.stringify(result, undefined, 3))
+  }, (err) => {
+        console.log('Something went wrong!')
+  })
+
+
+});
